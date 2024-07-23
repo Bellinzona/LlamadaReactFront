3c1e92837415458e36495b682307d6069bfc2c8b
MateoBellinzona <mtbellinzona@gmail.com>
2024-07-23 18:15:24 -0300

    server

--- src/CallApp.jsx
+++ src/CallApp.jsx
@@ -3,7 +3,7 @@ import io from 'socket.io-client';
 import Peer from 'simple-peer';
 import "./CallApp.css";
 
-const socket = io('http://localhost:4000');
+const socket = io('https://llamadareactsv.onrender.com');
 
 const CallApp = () => {
     const [me, setMe] = useState('');
